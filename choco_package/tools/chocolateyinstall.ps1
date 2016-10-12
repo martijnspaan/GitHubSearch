d31d3f4136d907fb9e72017a784421742bc0e381
Martijn Spaan <martijn.spaan@avivasolutions.nl>
2016-10-12 21:09:52 +0200

    Create chocolatey package

---
+++ choco_package/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName  = 'GitHubSearch'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://github.com/martijnspaan/GitHubSearch/releases/download/1.2.0/GitHubSearch_1.2.0.zip'
+$checksum     = '30BFEEF304FD311AEB880BE6FCF6B513'
+$checksumType = 'md5'
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir -checksum $checksum -checksumType $checksumType
+
+Install-ChocolateyPath $toolsDir 'Machine'
