944514576d7e2bf12de972538a2d72534568952d
Martijn Spaan <martijn.spaan@avivasolutions.nl>
2016-11-18 20:52:08 +0100

    Use choco template for replacing version and checksum

--- choco_template/tools/chocolateyinstall.ps1
+++ choco_template/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName  = 'GitHubSearch'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com/martijnspaan/GitHubSearch/releases/download/1.2.0/GitHubSearch_1.2.0.zip'
-$checksum     = '30BFEEF304FD311AEB880BE6FCF6B513'
+$url          = 'https://github.com/martijnspaan/GitHubSearch/releases/download/[[packageversion]]/GitHubSearch_[[packageversion]].zip'
+$checksum     = '[[checksum]]'
 $checksumType = 'md5'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir -checksum $checksum -checksumType $checksumType
---
+++ choco_template/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName  = 'GitHubSearch'
+
+# No need for un-installing since installation was done in package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
